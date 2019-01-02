00efd16f91dc27dc163c7a156ea6e8f47b8d1fdf
Exwe <postmaster@exwe.com>
2019-01-02 12:01:57 +0800

    test

--- app/UserLog.php
+++ app/UserLog.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserLog extends Model
 {
+
+    //create shell : php artisan make:model UserLog -m
     /**
      * The attributes that are mass assignable.
      *
