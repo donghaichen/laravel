f453012a49cd323f5ee3a01c5657c792766ec51f
donghaichen <chendonghai888@gmail.com>
2019-01-07 00:44:18 +0800

    'userInfo'

--- app/helpers.php
+++ app/helpers.php
@@ -63,7 +63,7 @@ function userLog($userId, $type, $content = '')
 //分页
 function perPage()
 {
-    if (isNull(request('per_page')) || request('per_page') < 0 || request('per_page') > 100)
+    if (request('per_page') == null || request('per_page') < 0 || request('per_page') > 100)
     {
         return 10;
     }
